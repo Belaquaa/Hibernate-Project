a07d2031960de6a6c107f98116cb963c6eb58ab1
belaquaa <belaquaa@mail.ru>
2024-04-30 01:02:43 +0300

    first commit

---
+++ src/main/java/jdbc/jpa/dao/UserDao.java
@@ -0,0 +1,19 @@
+package jdbc.jpa.dao;
+
+import jdbc.jpa.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void createUsersTable();
+
+    void dropUsersTable();
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void cleanUsersTable();
+}
